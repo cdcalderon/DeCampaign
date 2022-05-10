9dd8da528253b27372dd5eeac624c7affa438141
Carlos Calderon <cdcalderon@gmail.com>
2022-05-10 00:06:21 -0500

    Implement base contract, add contribute function

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.9;
 
 contract Campaign {
     address public manager;
+    uint256 public minimumContribution;
+    address[] public approvers;
+
+    constructor(uint256 _minimumContribution) {
+        manager = msg.sender;
+        minimumContribution = _minimumContribution;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
+    }
 }
