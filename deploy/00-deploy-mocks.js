62c850beedbd9137a5ed8193f50e2d5a7f5239a7
Carlos Calderon <cdcalderon@gmail.com>
2022-05-23 23:34:22 -0500

    test local env

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,3 +1,9 @@
 const { network } = require("hardhat");
 
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  // If we are on a local development network, we need to deploy mocks!
+};
 module.exports.tags = ["all", "mocks"];
