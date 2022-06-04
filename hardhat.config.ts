5fa95cf7c064c3b99a55182ed3db17480a425c64
Carlos Calderon <cdcalderon@gmail.com>
2022-06-03 23:32:16 -0500

    add configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
