63035337fe6c77f701a3d7d68a232a7a220542b4
Carlos Calderon <cdcalderon@gmail.com>
2022-06-07 21:12:49 -0500

    setup Campaing unit test with initial deployment

--- test/Campaign.js
+++ test/Campaign.js
@@ -9,12 +9,13 @@ const {
   ? describe.skip
   : describe("Campaign Unit Tests", () => {
       const chainId = network.config.chainId;
-      let campaignContractFactory, campaign;
+      let CampaignContract, campaign, deployer;
+      const MIN_CONTRIBUTION = ethers.utils.parseEther("0.01"); // 0.1 ETH
       beforeEach(async () => {
-        const { deployer } = await getNamedAccounts();
-        await deployments.fixture(["all"]);
-        campaignContractFactory = await ethers.getContractFactory("Campaign"); // Returns a new connection to the Campaign contract
-        campaign = await campaignContractFactory.deploy();
+        const accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        const CampaignContract = await ethers.getContractFactory("Campaign");
+        const campaign = await CampaignContract.deploy(MIN_CONTRIBUTION);
       });
 
       describe("constructor ", () => {
